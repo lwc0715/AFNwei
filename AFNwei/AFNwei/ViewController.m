fe4b645fdb677fa1990c284aa25433cb475aac39
liwenxia <2890973746@qq.com>
2016-02-02 16:08:43 +0800

    v2.0升级版

--- AFNwei/AFNwei/ViewController.m
+++ AFNwei/AFNwei/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"wwwwwwwwwww");
     NSLog(@"修改");
+     NSLog(@"第二次修改");
 }
 
 - (void)didReceiveMemoryWarning {
